c3e1e5267fb8b968d5865c1b2c0c3b2a5e06cb59
Peter Magenheimer <peter.magenheimer@gmail.com>
2015-03-15 17:54:42 +0100

    Add react-hot-loader.
    Run with:
    npm start
    
    http://gaearon.github.io/react-hot-loader/getstarted/
    https://github.com/gaearon/react-hot-boilerplate/blob/master/server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/main.js',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:3000',
+        'webpack/hot/dev-server',
+        './app/main.js'
+    ],
     output: {
+        path: path.join(__dirname, 'assets'),
+        publicPath: '/assets/',
         filename: 'bundle.js'       
     },
     module: {
         loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'] },
             { test: /\.scss$/, loader: 'style!css!sass' }
         ]
     },
     resolve: {
         extensions: ['', '.js', '.jsx'] 
-    }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ]
 };
