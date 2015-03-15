38df8de39795fd437a318f9fb997273ca17d1b0e
Peter Magenheimer <peter.magenheimer@gmail.com>
2015-03-15 18:18:02 +0100

    Initial add to list action (non-Fluxy for now).

--- app/components/ReadingListApp.jsx
+++ app/components/ReadingListApp.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import URLInput from './URLInput';
 
 export default React.createClass({
+    getInitialState: function () {
+        return {
+            items: []
+        };
+    },
+    handleAdd: function (text) {
+        var items = this.state.items;
+        items.push({
+            id: Date.now(),
+            text: text
+        });
+
+        this.setState({ items: items });
+    },
     render: function () {
+        var items = this.state.items.map(function (item) {
+            return (
+                <li key={item.id}>{item.text}</li>
+            );
+        });
+
         return (
             <div className="reading-list-app">
                 <h1>readinglist.co</h1>
-                <URLInput />
+                <URLInput handleAdd={this.handleAdd} />
+                <ul>
+                    {items}
+                </ul>
             </div>
         );
     }
--- app/components/URLInput.jsx
+++ app/components/URLInput.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
 export default React.createClass({
+    getInitialState: function () {
+        return {
+            text: ''
+        };
+    },
+    onChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        this.props.handleAdd(this.state.text);
+
+        this.setState({ text: '' });
+    },
     render: function () {
         return (
-            <input
-                className="url-input"
-                type="text"
-                placeholder="Paste a link" />
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    className="url-input"
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.onChange}
+                    placeholder="Paste a link" />
+                <button>Post</button>
+            </form>
         );
     }
 });
