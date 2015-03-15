3038333198cf745c0fdfba07e44db2e41257e8c9
Peter Magenheimer <peter.magenheimer@gmail.com>
2015-03-15 19:51:03 +0100

    Initial URL validation.

--- app/components/URLInput.jsx
+++ app/components/URLInput.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LinkedStateMixin from 'react/lib/LinkedStateMixin';
+import validUrl from 'valid-url';
 import LinkActions from '../actions/LinkActions';
 
 export default React.createClass({
@@ -13,17 +14,34 @@ export default React.createClass({
     },
     handleSubmit: function (e) {
         e.preventDefault();
-        LinkActions.addLink(this.state.text);
+        var text = this.state.text;
 
+        if (!validUrl.isUri(text)) {
+            return;
+        }
+
+        LinkActions.addLink(text);
         this.setState({ text: '' });
     },
+    isValidUri: function (e) {
+        return validUrl.isUri(this.state.text);
+    },
     render: function () {
+        var text = this.state.text;
+
+        var style = {
+            input: {
+                backgroundColor: text.length > 0 ? (validUrl.isUri(text) ? '#dff0d8' : '#f2dede') : null
+            }
+        };
+
         return (
             <form onSubmit={this.handleSubmit}>
                 <input
                     className="url-input"
                     type="text"
                     placeholder="Paste a link"
+                    style={style.input}
                     valueLink={this.linkState('text')} />
                 <button>Post</button>
             </form>
