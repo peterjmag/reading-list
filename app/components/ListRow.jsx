c30961925b5482c53da75ab2cf813d6a6a474148
Peter Magenheimer <peter.magenheimer@gmail.com>
2015-03-29 16:53:08 +0200

    List row: Add image placeholder and style text.

--- app/components/ListRow.jsx
+++ app/components/ListRow.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 export default React.createClass({
     render: function () {
         var title = this.props.link.title ?
-            (<strong>{this.props.link.title}<br /></strong>) :
-            null;
+            this.props.link.title :
+            '...';
 
         return (
             <div className="list-row">
-                {title}
-                {this.props.link.url}
+                <div className="image-container" />
+                <div className="text-container">
+                    <span className="link-title">{title}</span>
+                    <span className="link-url">{this.props.link.url}</span>
+                </div>
             </div>
         );
     }
